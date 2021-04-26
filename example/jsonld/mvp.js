df94e872781937497635aa650828017735296536
Doug <douganderson444@gmail.com>
2021-04-26 10:17:49 -0400

    add a bit to mvp

--- example/jsonld/mvp.js
+++ example/jsonld/mvp.js
@@ -1,10 +1,15 @@
-const IPFS = require('ipfs-core');
-const jsonld = require('jsonld');
+const IPFS = require('ipfs-core')
+const jsonld = require('jsonld')
 
-async function genCID() {
-    const ipfs = await IPFS.create();
-    const {cid} = await ipfs.add("Hello World!");
-    console.log(cid);
+async function genCID () {
+  const ipfs = await IPFS.create()
+  const { cid } = await ipfs.add('Hello World!')
+  console.log(cid.toString())
+  return cid
 }
 
-genCID().then(cid => {console.log(cid)});
+genCID().then(cid => {
+  console.log({ cid })
+
+  process.exit(0) // exit the node loop
+})
