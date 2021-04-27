43eb9f9266b1c92d90d817202cc00d9b4763c26b
Doug <douganderson444@gmail.com>
2021-04-27 14:48:55 -0400

    add hydroFile object constructor

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Dream API
+
+//  const CID = await hydroFile.track(ipfs.add("Hello world"), params)
+
+export class HydroFile {
+  constructor (id) {
+    this.id = id || 42
+    // ipfsNode.track = this.track
+  }
+}
