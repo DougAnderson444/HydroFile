86407b88595411219b72b3f5d43282040923f099
Doug <douganderson444@gmail.com>
2021-04-27 14:48:41 -0400

    add test framework

---
+++ test/all.test.mjs
@@ -0,0 +1,19 @@
+// test.mjs
+import { HydroFile } from '../src/index.js'
+import assert from 'assert'
+import chai from 'chai'
+const expect = chai.expect
+
+let hydroFile
+
+describe('Test', function () {
+  beforeEach(function () {
+    // runs once before the first test in this block
+  })
+
+  it('expect hydroFile id to be 42', () => {
+    hydroFile = new HydroFile()
+
+    expect(hydroFile.id).to.equal(42)
+  })
+})
